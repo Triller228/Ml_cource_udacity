f129b04fab59310764fb2804b82bded2f94063c4
Abu <triller.aliev@bk.ru>
2018-10-13 14:32:49 +0300

    Completed

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,14 +12,22 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
-
-
+features_train, features_test, labels_train, labels_test = preprocess() 
+clf = SVC(C = 10000,kernel="rbf")
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+count =0
+for i in range(len(pred)):
+    if (pred[i]==1):
+        count+=1
+print (count)
 
 
 #########################################################
--- tools/startup.py
+++ tools/startup.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
 
 print
-print "checking for nltk"
+print ("checking for nltk")
 try:
     import nltk
 except ImportError:
-    print "you should install nltk before continuing"
+    print ("you should install nltk before continuing")
 
 print "checking for numpy"
 try:
     import numpy
 except ImportError:
-    print "you should install numpy before continuing"
+    print ("you should install numpy before continuing")
 
 print "checking for scipy"
 try:
     import scipy
 except:
-    print "you should install scipy before continuing"
+    print ("you should install scipy before continuing")
 
-print "checking for sklearn"
+print ("checking for sklearn")
 try:
     import sklearn
 except:
